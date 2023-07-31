4f36c8261936c4dbba91a6a479f2b513c48fa6b8
BRaabe <benjamin.raabe@einsatzleitsoftware.de>
2023-07-31 14:57:01 +0200

    Re-Commit der config

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-  // Verbindungsdaten zu der Datenbank
-  $db_servername = "braabe.de";
-  $db_username = "d03cec87";
-  $db_password = "v36yxPgfRYEHfDxx";
-  $db_dbname = "d03cec87";
+  // Verbindungsdaten Datenbank
+  $db_servername = "";
+  $db_username = "";
+  $db_password = "";
+  $db_dbname = "";
 
   // Name der Organisation. Wird im Titel der Website angezeigt.
   $ORG_NAME = "W:R:S";
