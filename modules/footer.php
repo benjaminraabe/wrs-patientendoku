0ada68dc893d7333852a5202ef59bf30747b23fc
Benjamin Raabe <30860719+benjaminraabe@users.noreply.github.com>
2024-07-20 13:39:11 +0200

    Änderung der Lizenz auf AGPL v3 um Komplikationen mit der CC-Lizenz als Software-Lizenz zu vermeiden.

--- modules/footer.php
+++ modules/footer.php
@@ -1,5 +1,5 @@
 <div class="page-footer p-2 text-center no-print">
-  &copy; 2023 Benjamin Raabe als <a target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">CC ShareAlike (CC BY-SA 4.0)</a> <br>
+  &copy; 2024 Benjamin Raabe als <a target="_blank" href="https://www.gnu.org/licenses/agpl-3.0.txt">AGPL v3</a> [<a target="_blank" href="https://github.com/benjaminraabe/wrs-patientendoku">Quellcode</a>] <br>
   Projektleitung: Sebastian Rappmann in Zusammenarbeit mit W:R:S. <br>
   Unter Verwendung von <a href="https://github.com/olton/Metro-UI-CSS">Metro-UI-CSS (Serhii Pimenov, MIT)</a>, <a href="https://github.com/plotly/plotly.js/">Plotly.js (Plotly Inc, MIT)</a> und <a href="https://github.com/zxing-js/library">zxing-js (Apache 2.0)</a>.
 
